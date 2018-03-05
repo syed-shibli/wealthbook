9aaf3f0b79791207a59d906c93aebbc87b8d2e79
daffolapmac <mukesh.yadav@daffodilsw.com>
2018-03-05 09:56:21 +0530

    Add splash screen

---
+++ app/src/main/java/com/example/daffolapmac/wealthbook/screen/splash/view/ISplashView.java
@@ -0,0 +1,9 @@
+package com.example.daffolapmac.wealthbook.screen.splash.view;
+
+public interface ISplashView {
+
+    /**
+     * Called by presenter when timer expire.
+     */
+    void onSplashTimeOut();
+}
